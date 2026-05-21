adec9e11d4e4d691f0cb7d9d97805e22639832b7
MRIMan47201 <56274774-MRIMan47201@users.noreply.replit.com>
2026-05-21 00:14:23 +0000

    Restored to '5b9e5807dd41c02665f292e49983b046ddc1a4fb'
    Replit-Restored-To: 5b9e5807dd41c02665f292e49983b046ddc1a4fb

--- artifacts/mobile/metro.config.js
+++ artifacts/mobile/metro.config.js
@@ -1,9 +1,3 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-const config = getDefaultConfig(__dirname);
-
-config.resolver.blockList = [
-  /node_modules\/.*\/vite_tmp_.*/,
-];
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
